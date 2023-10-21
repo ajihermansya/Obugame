fb45e2f86e8d90f94698614055a96ce9b59df179
aji_hermansya <ajihermansya14@gmail.com>
2023-10-21 23:51:00 +0700

    start pembuatan : home-obugame

--- app/src/main/java/com/rumahproduksi/obugame/HomePageActivity.kt
+++ app/src/main/java/com/rumahproduksi/obugame/HomePageActivity.kt
@@ -2,10 +2,21 @@ package com.rumahproduksi.obugame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.rumahproduksi.obugame.databinding.ActivityHomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val actionBar: ActionBar? = supportActionBar
+        if (actionBar != null) {
+            actionBar.hide()
+        }
+
+
     }
 }
