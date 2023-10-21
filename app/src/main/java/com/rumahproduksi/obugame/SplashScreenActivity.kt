4a77b8a1242a9378ec0860d8f3a891eded5eb225
aji_hermansya <ajihermansya14@gmail.com>
2023-10-21 23:19:15 +0700

    splash screnn-done

--- app/src/main/java/com/rumahproduksi/obugame/SplashScreenActivity.kt
+++ app/src/main/java/com/rumahproduksi/obugame/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.rumahproduksi.obugame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this@SplashScreenActivity, HomePageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
