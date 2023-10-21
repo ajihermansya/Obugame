8f0d5934ee54fee31a00a5d7c955ed1ada80fc45
aji_hermansya <ajihermansya14@gmail.com>
2023-10-22 02:52:14 +0700

    start pembuatan : fragment done

--- app/src/main/java/com/rumahproduksi/obugame/HomePageActivity.kt
+++ app/src/main/java/com/rumahproduksi/obugame/HomePageActivity.kt
@@ -3,7 +3,13 @@ package com.rumahproduksi.obugame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.rumahproduksi.obugame.adapter.adapter_fragment.DetailAdapter
 import com.rumahproduksi.obugame.databinding.ActivityHomePageBinding
+import com.rumahproduksi.obugame.fragmenactivity.BahanBakuFragment
+import com.rumahproduksi.obugame.fragmenactivity.BahanLainnyaFragment
+import com.rumahproduksi.obugame.fragmenactivity.BiayaLainFragment
 
 class HomePageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomePageBinding
@@ -18,5 +24,26 @@ class HomePageActivity : AppCompatActivity() {
         }
 
 
+        val fragments = mutableListOf<Fragment>(
+            BahanBakuFragment(),
+            BahanLainnyaFragment(),
+            BiayaLainFragment()
+        )
+        val titleFragments = mutableListOf(
+            getString(R.string.bahanbaku), getString(R.string.bahanlainnya), getString(R.string.biayalainnya)
+        )
+
+
+        val adapter = DetailAdapter(this, fragments)
+        binding.viewpager.adapter = adapter
+
+        TabLayoutMediator(binding.tab,binding.viewpager) { tab, posisi ->
+            tab.text = titleFragments[posisi]
+        }.attach()
+
+
     }
+
+
+
 }
