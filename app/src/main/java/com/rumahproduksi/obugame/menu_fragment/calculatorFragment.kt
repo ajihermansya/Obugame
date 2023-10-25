1ee11129bd57556a065c9ecaf9debaea70f3c5ca
aji_hermansya <ajihermansya14@gmail.com>
2023-10-26 00:22:11 +0700

    nyimpan data firebase eoq

--- app/src/main/java/com/rumahproduksi/obugame/menu_fragment/calculatorFragment.kt
+++ app/src/main/java/com/rumahproduksi/obugame/menu_fragment/calculatorFragment.kt
@@ -3,6 +3,7 @@ package com.rumahproduksi.obugame.menu_fragment
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.rumahproduksi.obugame.R
 import com.rumahproduksi.obugame.SettingActivity
+import com.rumahproduksi.obugame.adapter.dataclass_model.BahanBaku
+import com.rumahproduksi.obugame.adapter.dataclass_model.CalculatorModel
 import com.rumahproduksi.obugame.adapter.logic.EOQCalculatorAdapter
 import com.rumahproduksi.obugame.databinding.FragmentCalculatorBinding
 import java.text.SimpleDateFormat
@@ -25,10 +28,14 @@ class calculatorFragment : Fragment() {
     lateinit var binding: FragmentCalculatorBinding
     private val eoqCalculatorAdapter = EOQCalculatorAdapter()
     private lateinit var mDbRef: DatabaseReference
+    private lateinit var status: TextView
+    private lateinit var hasilEoq: TextView
+    private var dataSudahDisimpan = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         binding = FragmentCalculatorBinding.inflate(inflater, container, false)
 
@@ -36,8 +43,9 @@ class calculatorFragment : Fragment() {
         val inputBahanBaku = binding.root.findViewById<EditText>(R.id.input_bahanbaku)
         val inputKemasanTerpakai = binding.root.findViewById<EditText>(R.id.input_kemasanterpakai)
         val inputHargaKemasan = binding.root.findViewById<EditText>(R.id.input_hargakemasan)
-        val hasilEoq = binding.root.findViewById<TextView>(R.id.hasil_eoq)
-        val status = binding.root.findViewById<TextView>(R.id.status)
+
+        hasilEoq = binding.root.findViewById(R.id.hasil_eoq)
+        status = binding.root.findViewById(R.id.status)
         val hitungEoqButton = binding.root.findViewById<Button>(R.id.hitung_eoq)
 
         binding.settingApp.setOnClickListener {
@@ -56,27 +64,8 @@ class calculatorFragment : Fragment() {
             val hargaKemasan = inputHargaKemasan.text.toString().toIntOrNull()
             val tanggal = inputTanggal.text.toString()
 
-            if (beratBahan != null && jumlahKemasan != null && hargaKemasan != null && tanggal.isNotEmpty()) {
-                val hasilPerhitungan = eoqCalculatorAdapter.calculateEOQ(beratBahan, jumlahKemasan, hargaKemasan)
-                hasilEoq.text = hasilPerhitungan.toString()
-
-                if (hasilPerhitungan == 29.0) {
-                    status.text = "Perhitungan EOQ Optimal"
-                } else if (hasilPerhitungan < 29.0) {
-                    status.text = "Perhitungan EOQ Kurang Optimal"
-                } else {
-                    status.text = "Perhitungan EOQ Berlebih"
-                }
-            } else {
-                status.text = "Semua input harus diisi"
-                Toast.makeText(context, "Semua Inputan Harus Terisi", Toast.LENGTH_SHORT).show()
-            }
-
-
+            tambahHasil(beratBahan, jumlahKemasan, hargaKemasan, tanggal )
         }
-
-
-
 
         return binding.root
     }
@@ -105,12 +94,44 @@ class calculatorFragment : Fragment() {
         val dateFormat = SimpleDateFormat("dd-MM-yyyy")
         return dateFormat.format(calendar.time)
     }
+    private fun tambahHasil(beratBahan: Float?, jumlahKemasan: Int?, hargaKemasan: Int?, tanggal: String) {
+        if (beratBahan != null && jumlahKemasan != null && hargaKemasan != null && tanggal.isNotEmpty()) {
+            val hasileoq = eoqCalculatorAdapter.calculateEOQ(beratBahan, jumlahKemasan, hargaKemasan)
+            hasilEoq.text = hasileoq.toString()
 
+            val statusText = if (hasileoq == 29.0) {
+                "Perhitungan EOQ Optimal"
+            } else if (hasileoq < 29.0) {
+                "Perhitungan EOQ Kurang Optimal"
+            } else {
+                "Perhitungan EOQ Berlebih"
+            }
+            status.text = statusText
 
+            if (!dataSudahDisimpan) {
+                // Menambahkan data ke Firebase
+                val firebaseDatabase = FirebaseDatabase.getInstance()
+                val mDbRef = firebaseDatabase.getReference("hasil_perhitungan")
 
+                val newCalculatorModel = CalculatorModel(
+                    beratBahan.toString(),
+                    jumlahKemasan.toString(),
+                    hargaKemasan.toString(),
+                    hasileoq.toString(),
+                    tanggal
+                )
+                val newBahanBakuRef = mDbRef.push()
+                newBahanBakuRef.setValue(newCalculatorModel)
+                Toast.makeText(context, "Data berhasil ditambahkan ke Firebase", Toast.LENGTH_SHORT)
+                    .show()
+                dataSudahDisimpan = true
+            }
+        } else {
+            status.text = "Semua input harus diisi"
+            Toast.makeText(context, "Semua Inputan Harus Terisi", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
-
-
 
 
